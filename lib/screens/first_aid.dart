dcb74e1e9cc63c5812268d0f0f33c2a1c212b78c
Ayako1962 <ayakobilhah@gmail.com>
2023-02-04 15:13:51 +0300

    added trackrecord

--- lib/screens/first_aid.dart
+++ lib/screens/first_aid.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Firstaid extends StatelessWidget {
   const Firstaid({super.key});
@@ -19,20 +20,38 @@ class Firstaid extends StatelessWidget {
         ),
         ),
         body: SingleChildScrollView(
+          
           child: Column(
             children: <Widget>[
-              ListTile(title: Text("-Ease the person to the floor"),),
-                ListTile(title: Text("-Turn the person gently onto one side"),),
-                  ListTile(title: Text("-Clear the area around the person to prevent any injury"),),
-                    ListTile(title: Text("-Put something soft and flat under his or her head"),),
-                      ListTile(title: Text("-Remove eye glasses"),),
-                        ListTile(title: Text("-Loosen tie or anything around the neck that could prevent them from breathing well"),),
-                        
-               ListTile(title: Text("DO NOT"),leading: Icon(Icons.do_disturb_alt),),
-               ListTile(title: Text("-Do not put anything in the person's mouth"),),
-               ListTile(title: Text("-Do not hold the person down or try to stop his or her movements"),),
-               ListTile(title: Text("-Do not try to give mouth to mouth breaths"),),
-               ListTile(title: Text("-Do not offer the person water or food until she or he is fully alert"),),
+              Container(
+              width: Get.width,
+              height: Get.height * 0.3,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/faidbg.png"),
+                  fit: BoxFit.fill,
+                ),
+              
+              ),
+            ),
+              ListTile(title: Text("1). Ease the person to the floor"), textColor: Colors.teal),
+                ListTile(title: Text("2). Turn the person gently onto one side"), textColor: Colors.teal),
+                  ListTile(title: Text("3). Clear the area around the person to prevent any injury"), textColor: Colors.teal),
+                    ListTile(title: Text("4). Put something soft and flat under his or her head"), textColor: Colors.teal),
+                      ListTile(title: Text("5). Remove eye glasses"), textColor: Colors.teal),
+                        ListTile(title: Text("6). Loosen tie or anything around the neck that could prevent them from breathing well"), textColor: Colors.teal),
+                         ListTile(title: Text("7). loosen neckware"), textColor: Colors.teal),
+ ListTile(title: Text("8). look for a medical ID tag"), textColor: Colors.teal),
+ ListTile(title: Text("9). stay back if the person appears angry or aggressive"), textColor: Colors.teal),
+ ListTile(title: Text("10).STAY CALM"), textColor: Colors.teal),
+
+
+
+               ListTile(title: Text("DO NOT"),leading: Icon(Icons.do_disturb_alt), textColor: Colors.black),
+               ListTile(title: Text("-Do not put anything in the person's mouth"),textColor: Colors.red),
+               ListTile(title: Text("-Do not hold the person down or try to stop his or her movements"),textColor: Colors.red),
+               ListTile(title: Text("-Do not try to give mouth to mouth breaths"),textColor: Colors.red),
+               ListTile(title: Text("-Do not offer the person water or food until she or he is fully alert"),textColor: Colors.red),
             ],
           ),
           
--- lib/screens/track_record.dart
+++ lib/screens/track_record.dart
@@ -1,11 +1,91 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/widgets.dart';
 
 class Trackrecord extends StatelessWidget {
-  const Trackrecord({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return LineChart(
+      LineChartData(
+        borderData:
+            FlBorderData(border: Border.all(color: Colors.white, width: 0.5)),
+        gridData: FlGridData(
+          drawHorizontalLine: false,
+        ),
+        titlesData: FlTitlesData(
+          bottomTitles: AxisTitles(
+              sideTitles: SideTitles(
+            showTitles: true,
+            getTitlesWidget: (value, meta) {
+              var text = "";
+
+              switch (value.toInt()) {
+                case 1:
+                  text = 'Jan';
+                  break;
+                case 2:
+                  text = 'Feb';
+                  break;
+                case 3:
+                  text = 'Mar';
+                  break;
+                case 4:
+                  text = 'Apr';
+                  break;
+                case 5:
+                  text = 'May';
+                  break;
+                case 6:
+                  text = 'Jun';
+                  break;
+                case 7:
+                  text = 'Jul';
+                  break;
+                case 8:
+                  text = 'Aug';
+                  break;
+                case 9:
+                  text = 'Sep';
+                  break;
+                case 10:
+                  text = 'Oct';
+                  break;
+                case 11:
+                  text = 'Nov';
+                  break;
+                case 12:
+                  text = 'Dec';
+                  break;
+              }
+
+              return Text(
+                text,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold),
+              );
+            },
+          )),
+          leftTitles: AxisTitles (sideTitles:SideTitles(
+            interval: 4,
+            showTitles: true,
+            getTitlesWidget: (value, meta) {
+              var text=  (value.toInt() == 0)?'':value.toInt().toString();
+
+
+              return Text(text,style: const TextStyle(
+                color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),);
+            },
+           
+          ),
+        )),
+        minX: 1,
+        minY: 0,
+        maxX: 12,
+        maxY: 20,
+       // lineBarsData: lineChartBarData,
+      ),
+    );
   }
 }
